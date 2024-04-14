ac2663e94887292fb22c4f3b1c4a3a9af979d75f
sieber2210 <jsieber53@gmail.com>
2024-04-13 21:36:19 -0400

    Updated character system to be abstract based
    Updated character parent to be abstract.
    Removed some unneeded scripts for new parent type.
    Updated existing character children to use the new system.
    Added test keys on the player script to test all methods from the character parent. (Space - Start turn | 1 - Attack | 2 - Heal | 3 - End Turn)

--- Assets/Scripts/Characters/Enemies/Skeleton.cs
+++ Assets/Scripts/Characters/Enemies/Skeleton.cs
@@ -2,15 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Skeleton : MonoBehaviour
+public class Skeleton : Character
 {
     private void Start()
     {
-        EnemyCharacter skelly = new EnemyCharacter();
-        Debug.Log("Creating " + skelly.name + "...");
-        
-        Debug.Log(skelly.name + "created!");
+        characterName = "Skeleton";
+        attackDamage = 5f;
+        healAmount = 1f;
+    }
 
-        skelly.Attack();
+    public override void StartTurn()
+    {
+        base.StartTurn();
+        //do extra skeleton stuff on start of turn
+    }
+
+    public override void Attack()
+    {
+        base.Attack();
+        //do extra skeleton attack stuff
+    }
+
+    public override void Heal()
+    {
+        base.Heal();
+        //do extra skeleton heal stuff
+    }
+
+    public override void EndTurn()
+    {
+        base.EndTurn();
+        //do extra skeleton stuff on end of turn
     }
 }
--- Assets/Scripts/Characters/Player/PlayerCharacter.cs
+++ Assets/Scripts/Characters/Player/PlayerCharacter.cs
@@ -4,11 +4,46 @@ using UnityEngine;
 
 public class PlayerCharacter : Character
 {
-    public PlayerCharacter()
+    private void Start()
     {
-        attackSpeed = 2.5f;
-        moveSpeed = 5f;
+        characterName = "Player";
         attackDamage = 10f;
-        name = "Player";
+        healAmount = 5f;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            StartTurn();
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            Attack();
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+            Heal();
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+            EndTurn();
+    }
+
+    public override void StartTurn()
+    {
+        base.StartTurn();
+        //do extra player stuff on start of turn
+    }
+
+    public override void Attack()
+    {
+        base.Attack();
+        //do extra player attack stuff
+    }
+
+    public override void Heal()
+    {
+        base.Heal();
+        //do extra player heal stuff
+    }
+
+    public override void EndTurn()
+    {
+        base.EndTurn();
+        //do extra player stuff on end of turn
     }
 }
